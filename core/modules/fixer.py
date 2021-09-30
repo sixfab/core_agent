c506b9561e050c284f4d6c6195266245cb1b0089
Bugra Isguzar <biisguzar@gmail.com>
2021-09-30 19:26:21 +0300

    Add fixer for new dependencies

--- core/modules/fixer.py
+++ core/modules/fixer.py
@@ -17,3 +17,4 @@ def execute_fix(name, command):
 execute_fix("190320-manager-ifmetric", r'while :;do command -v ifmetric;if [ $? -eq "0" ];then sudo systemctl restart core_manager;break;fi;sudo apt-get install ifmetric -y;done')
 execute_fix("150421-plugdev-group.2", r'PERMISSIONS_TO_ADD="SUBSYSTEM==\"usb\", ENV{DEVTYPE}==\"usb_device\", MODE=\"0664\", GROUP=\"plugdev\"";PLUGDEV_RULES_PATH=/etc/udev/rules.d/plugdev_usb.rules;if [ ! -f $PLUGDEV_RULES_PATH ];then echo $PERMISSIONS_TO_ADD|sudo tee $PLUGDEV_RULES_PATH>/dev/null 2>&1;sudo udevadm control --reload;sudo udevadm trigger;fi;sudo usermod -aG gpio sixfab>/dev/null 2>&1;sudo usermod -aG plugdev sixfab>/dev/null 2>&1;sudo systemctl restart core_manager')
 execute_fix("210621-update-atcom", r'sudo pip3 install -U atcom')
+execute_fix("301021-networking-dependencies", r'if ! [ -x "$(command -v route)" ]; then sudo apt-get install net-tools -y; fi; if ! [ -x "$(command -v lshw)" ]; then sudo apt-get install lshw -y; fi')
