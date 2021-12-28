ce88fb7d26a35ea0690747caddb474ba792505f4
Yasin Kaya <yasinkaya.121@gmail.com>
2021-12-28 17:12:16 +0300

    Improve readibility

--- core/helpers/yamlio.py
+++ core/helpers/yamlio.py
@@ -1,22 +1,21 @@
-
-import yaml
 import os.path
+import yaml
 
 ENV_PATH = "/opt/sixfab/.env.yaml"
 USER_FOLDER_PATH =  os.path.expanduser("~")
-CORE_FOLDER_PATH = USER_FOLDER_PATH + "/.core/"
-SYSTEM_PATH = CORE_FOLDER_PATH + "system.yaml"
-BULK_CACHE= f"{USER_FOLDER_PATH}/.core/bulk.yaml"
+CORE_FOLDER_PATH = f"{USER_FOLDER_PATH}/.core/"
+SYSTEM_PATH = f"{CORE_FOLDER_PATH}/system.yaml"
+BULK_CACHE= f"{CORE_FOLDER_PATH}/bulk.yaml"
 
 def read_yaml_all(file):
-    with open(file) as f:
-        data = yaml.safe_load(f)
+    with open(file) as yaml_file:
+        data = yaml.safe_load(yaml_file)
         return data or {}
 
 def write_yaml_all(file, items, clear = True):
-    if clear == True:
-        with open(file, 'w') as f:
-            yaml.dump(items, f, default_flow_style=False)
+    if clear is True:
+        with open(file, 'w') as yaml_file:
+            yaml.dump(items, yaml_file, default_flow_style=False)
     else:
-        with open(file, 'a') as f:
-            yaml.dump(items, f, default_flow_style=False)
+        with open(file, 'a') as yaml_file:
+            yaml.dump(items, yaml_file, default_flow_style=False)
